ceb30b7e3b1224a962800e333ecbf8d9db3903ee
Jack Short <jack.short1408@gmail.com>
2024-04-30 20:52:27 -0400

    updates

--- fire_escape_new.py
+++ fire_escape_new.py
@@ -6,21 +6,20 @@ pygame.init()
 
 pygame.display.set_caption("Fire Escape")
 
-new_var = 5
-
 
 def update_screen(
     screen: pygame.Surface,
     red: tuple[int, int, int],
     blue: tuple[int, int, int],
     fire: pygame.Rect,
-    rain: pygame.Rect,
+    rain_drops: list[pygame.Rect],
     rain_counter: list[int],
 ):
     white = (255, 255, 255)
     screen.fill(white)
     pygame.draw.rect(screen, red, fire)
-    pygame.draw.rect(screen, blue, rain)
+    for rain in rain_drops:
+        pygame.draw.rect(screen, blue, rain)
     pygame.display.update()
 
 
@@ -39,21 +38,22 @@ def fire_movement(
 
 
 def rain_movement(
-    rain: pygame.Rect,
+    rain_drops: list[pygame.Rect],
     rain_vel: list[int],
     screen_width: int,
     screen_height: int,
     rain_counter: list[int],
 ):
-    rain.y += rain_vel[0]
-    if rain.y > screen_height:
-        rain.x = random.randint(0, screen_width - rain.width)
-        rain.y = -rain.height
-        rain_counter[0] += 1
+    for rain in rain_drops:
+        rain.y += rain_vel[0]
+        if rain.y > screen_height:
+            rain.x = random.randint(0, screen_width - rain.width)
+            rain.y = -rain.height
+            rain_counter[0] += 1
         print(rain_counter)
-    if rain_counter[0] % 10 == 0 and rain_counter[0] != 0:
-        rain_vel[0] += 1
-        rain_counter[0] += 1
+        if rain_counter[0] % 10 == 0 and rain_counter[0] != 0:
+            rain_vel[0] += 1
+            rain_counter[0] += 1
 
 
 def main():
@@ -82,31 +82,44 @@ def main():
     # RAIN
     rain_width = 10
     rain_height = 10
-    rain_vel = [3]
+    rain_vel = [2]
     rain_counter = [0]
 
-    rain = pygame.Rect(
-        random.randint(0, random.randint(0, 500)),
-        random.randint(0, random.randint(0, 50)),
-        rain_width,
-        rain_height,
-    )
+    timer = 0
 
+    rain_drops = [
+        pygame.Rect(
+            random.randint(0, random.randint(0, screen_width - rain_width)),
+            -rain_height,
+            rain_width,
+            rain_height,
+        )
+        for _ in range(1)
+    ]
     run = True
     while run:
+        timer += 1
         clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        for rain in rain_drops:
+            if rain.colliderect(fire):
+                run = False
+                print("GAME OVER")
+                pygame.time.delay(5000)
+        if timer % 1000 == 0:
+            new_rain_drop = pygame.Rect(
+                random.randint(0, random.randint(0, screen_width - rain_width)),
+                -rain_height,
+                rain_width,
+                rain_height,
+            )
+            rain_drops.append(new_rain_drop)
 
-        if rain.colliderect(fire):
-            run = False
-            print("GAME OVER")
-            pygame.time.delay(5000)
-
-        update_screen(screen, red, blue, fire, rain, rain_counter)
+        update_screen(screen, red, blue, fire, rain_drops, rain_counter)
         fire_movement(fire, fire_vel, screen_width, screen_height)
-        rain_movement(rain, rain_vel, screen_width, screen_height, rain_counter)
+        rain_movement(rain_drops, rain_vel, screen_width, screen_height, rain_counter)
 
     pygame.quit()
 
